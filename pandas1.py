2039d032340ba2666b0081060c316201d97af323
DiboraEyasuE <diboraeyasu792@gmail.com>
2025-10-21 20:12:02 +0300

    second commit

---
+++ pandas1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+RGB = pd.DataFrame({"Red" :[255, 0, 0], "Green" : [0, 255, 0], "Blue" : [0, 0, 255]})
+fruit_sales = pd.DataFrame({"Apples" : [35, 41], "Bananas" : [21, 34]}, index = ['2017 Sales', '2018 Sales'])
+ingredients = pd.Series(['4 cups', '1 cup', '2 large', '1 can'], index = ['Flour', 'Milk', 'Eggs', 'Spam'], name='Dinner')
+
+survey = pd.read_csv("enterprise.csv")
+print(survey.shape)
+
+print(RGB)
+print(ingredients)
