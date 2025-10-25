8b8d6f4a07e1f3171915100b9bf62c699d836de8
DiboraEyasuE <diboraeyasu792@gmail.com>
2025-10-25 23:59:11 +0300

    commit3

--- enterprz.py
+++ enterprz.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 survey = pd.read_csv("enterprise.csv")
-#print(survey.head())
+print(survey.head())
 print(survey.shape)
 sliced = survey.iloc[-5:].notnull()
 survey.rename(columns={'industry_code_ANZSIC':'ind_cd_ANZSIC'})
